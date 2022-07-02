fe8cd50d8c067d8d4667a1f1fd51a7a73ff8cfd3
Nasirullah Oladipo <60460558+softcruder@users.noreply.github.com>
2022-07-02 13:39:39 +0100

    A function to return of numbers in an Array.

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
+    let sumOfNumbers = 0;
+
+    for (let i = 0; i < arrayOfNumbers.length; i++)
+    sumOfNumbers += arrayOfNumbers[i]
+
     return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+    assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
